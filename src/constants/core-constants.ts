8713109328751f7a7960d36a09b2282e8eaf9cf8
lizuokuan <827800024@qq.com>
2023-06-09 14:12:04 +0800

    增加jc-util,增加token-util

---
+++ src/constants/core-constants.ts
@@ -0,0 +1,6 @@
+// 管理端-Token关键字
+const TOKEN_KEY = "AdminToken";
+
+export {
+    TOKEN_KEY
+}
---
+++ src/utils/store/token-utils.ts
@@ -0,0 +1,33 @@
+import Cookies from '@jcstdio/jc-utils'
+import {TOKEN_KEY} from "@/constants/core-constants";
+
+// @ts-ignore
+const cookies: Cookies = new Cookies();
+
+/**
+ * 获取管理端Token
+ */
+const getAdminToken = () => {
+    return cookies.get(TOKEN_KEY)
+}
+
+/**
+ * 设置管理端token
+ * @param token token值
+ */
+const setAdminToken = (token: string) => {
+    return cookies.set(TOKEN_KEY, token)
+}
+
+/**
+ * 移除Token
+ */
+const remoteAdminToken = () => {
+    return cookies.remove(TOKEN_KEY)
+}
+
+export {
+    getAdminToken,
+    setAdminToken,
+    remoteAdminToken
+}
