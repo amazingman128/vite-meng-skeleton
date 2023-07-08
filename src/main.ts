00dc6341e6a42a059a6915bdd69020b713f741fa
lizuokuan <827800024@qq.com>
2023-07-08 16:16:16 +0800

    添加代码预览插件

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import {createApp} from 'vue'
 import ElementPlus from 'element-plus'
+import CodeBlock from 'vue3-code-block';
 import 'element-plus/dist/index.css'
 import 'normalize.css'
 import '@unocss/reset/normalize.css'
@@ -9,6 +10,7 @@ import router from '@/router/index'
 import 'uno.css'
 
 const app = createApp(App)
+app.component('CodeBlock', CodeBlock)
 app.use(ElementPlus)
 app.use(router)
 app.mount('#app')
